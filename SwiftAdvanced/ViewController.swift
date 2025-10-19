0eecf7f2b4475c856a215ac02ba2ef9967ec2b85
Ruslan <rus.baranovskiy@gmail.com>
2025-10-19 22:24:02 +0300

    Initial Commit

---
+++ SwiftAdvanced/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftAdvanced
+//
+//  Created by Руслан Барановский on 19.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
